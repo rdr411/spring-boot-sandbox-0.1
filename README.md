289824367216a8f3b4cd2938414bc5d755981a37
russelr <rich@upimedia.net>
2016-12-18 11:36:33 +0000

    Local commit
    Local copy 18/12/16

---
+++ README.md
@@ -0,0 +1,56 @@
+spring-boot-sandbox
+====
+
+A simple Spring Boot project for experimenting with the framework.
+
+The application uses Jetty rather than the default Tomcat Web server.
+
+The root endpoint outputs a parameterised message. The parameters are picked up from Java 
+system properties and environment variables.
+
+The project has Docker file and a Heroku Procfile and .env file.
+
+Build
+----
+```
+mvn clean package
+```
+
+Run
+----
+```
+java -jar target/spring-boot-sandbox-1.0-SNAPSHOT.jar
+or
+mvn spring-boot:run
+```
+
+Endpoints
+----
+Application Root - http://<host>:<port>/
+
+Run locally in Docker container
+----
+```
+./run-docker-image.sh
+http://localhost:8080/
+```
+
+Run on Heroku
+----
+```
+heroku create
+git push heroku master
+heroku ps:scale web=1
+./run-docker-image.sh
+heroku open
+```
+
+Test Locally with Heroku
+----
+```
+heroku local web
+http://localhost:5000/
+```
+
+Note the .env file provides the environment variables when running Heroku locally.
+
---
+++ src/main/java/uk/co/redsoft/springboot/ExampleController.java
@@ -0,0 +1,38 @@
+package uk.co.redsoft.springboot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class ExampleController {
+
+    @Value("#{systemEnvironment['MESSAGE']}")
+    private String message;
+
+    @RequestMapping("/")
+    public ExampleResponse home() {
+        return new ExampleResponse("Java system property (server.port): [" + System.getProperty("server.port")
+                                 + "]. Environment Variable via System.getenv(MESSAGE): [" + System.getenv("MESSAGE")
+                                 + "]. Environment Variable via @Value annotation (MESSAGE): [" + message + "]");
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ExampleController.class, args);
+    }
+    
+    private static class ExampleResponse {
+        private final String message;
+
+        public ExampleResponse(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
+}
